b30331671164c5add71971ff57647e20a519f716
prijatelj <theomnipotentwoof@gmail.com>
2021-11-08 14:40:50 -0500

    WIP: init par clip enc
    Former-commit-id: 6a776419819f249dbd0a227afcbd9b75ec40b561 [formerly 088f92e0edfc162e572228ef76e34d8db271672c] [formerly b309f878a2e2ed504db75ec21d0300cbc192a233] [formerly c4ca56af1db0ea4b3ceea7c7020310d2471c5fb7 [formerly c4ca56af1db0ea4b3ceea7c7020310d2471c5fb7 [formerly bcc4fa2a2284e58be77bf1400db8d5281f06c46c]]]
    Former-commit-id: a38bf3284fdc9eca3ef4e6025562c1384342c741 [formerly a5a8676c7e95fc512e2f5232d50883b9f9474dbe]
    Former-commit-id: 1397982ba587ad1c84ebb43282dcdf11dd0653a9
    Former-commit-id: 4991b6917041010389e94f4cc9ea63e995f7fe35

---
+++ arn/scripts/crc/clip_encode_par.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#$ -pe smp 4
+#$ -N clipEncPAR
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -o $HOME/scratch_48/har/kinetics/clip_encode_pred/logs/par/
+#$ -e $HOME/scratch_48/har/kinetics/clip_encode_pred/logs/par/
+#$ -t 1-6
+
+# Check if given an arg
+if [[ "$#" -eq 1 ]]; then
+    SGE_TASK_ID="$1"
+    echo "Given an argument and thus set SGE_TASK_ID to $SGE_TASK_ID"
+fi
+
+echo "SGE_TASK_ID = $SGE_TASK_ID"
+
+# Set up environment
+module add conda
+conda activate arn
+
+# Profile the CRC Job on the node in which it is running.
+crc_profile
+
+# Path variables
+BASE="/scratch365/dprijate/osr/har"
+DATA="$BASE/data/par"
+VIDS_BASE="/afs/crc.nd.edu/group/cvrl/scratch_34/sgrieggs"
+
+DSET_NUM="par"
+
+AUG=""
+
+# Add to path variables for this given run
+if [ "$SGE_TASK_ID" -eq "1" ]; then
+    # Simply encode all of training together.
+    SPLIT="train"
+    VIDS="$VIDS_BASE/all_vids_and_augs"
+
+# Additional videos: Encode in their batches as they are.
+elif [ "$SGE_TASK_ID" -eq "4" ]; then
+    SPLIT="train"
+elif [ "$SGE_TASK_ID" -eq "5" ]; then
+    SPLIT="validate"
+elif [ "$SGE_TASK_ID" -eq "6" ]; then
+    SPLIT="test"
+else
+    echo "ERROR: Unexpected SGE_TASK_ID: $SGE_TASK_ID"
+    exit 1
+fi
+
+python3 "$BASE/arn/arn/scripts/research/clip/kinetics_clip_img_encode.py" \
+    "$VIDS/" \
+    "$DATA/$SPLIT.json" \
+    "$DATA/unique_labels.txt" \
+    "$BASE/models/clip/clip_ViT-B_32.pt" \
+    --label_path "$DATA/clip_encoded_k$DSET_NUM"_classes.pt \
+    --image_path "$DATA/clip/$SPLIT/$AUG/images_enc_clip_ViT-B_32.pt" \
+    --pred_path "$DATA/clip/$SPLIT/$AUG/preds_K$DSET_NUM"_clip_ViT-B_32_preds.pt
