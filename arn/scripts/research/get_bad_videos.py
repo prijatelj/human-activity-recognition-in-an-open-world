3eb847eea1737916bdd59bfa1b8c44b2b3ac9189
prijatelj <theomnipotentwoof@gmail.com>
2021-11-10 01:00:18 -0500

    WIP: adding get bad videos script to test KineticsUnified Dataset with Dataloader.
    Former-commit-id: a75f245014e1d6a8bb5cb43f1efce2ba72a704a9 [formerly ef167623c6c4d46084519319fe9836963ba6bb62] [formerly 2e2ef1aba4cc7ac5cad971cb9d6ede5321bfd1dc] [formerly 116cb46300449d1fa038bd2028388f7d4230fa56 [formerly 116cb46300449d1fa038bd2028388f7d4230fa56 [formerly 3f87301c8423be1ace5086507ffb8b457562f8f4]]]
    Former-commit-id: 60e8497a945568b89ddabb0ffd90572866fdd223 [formerly 910a44f789446926701ac1b4d754429e322648d1]
    Former-commit-id: 71a1dfc559799411c5e5b340fbf51b30c7631a20
    Former-commit-id: 68a78dfc453bceb7071bcf59a536b1432137af50

---
+++ arn/scripts/research/get_bad_videos.py
@@ -0,0 +1,77 @@
+"""A Script to save a list of bad videos, whether missing or corrupted."""
+import os
+
+import torch
+from tqdm import tqdm
+
+from arn.data.kinetics_unified import (
+    KineticsUnified,
+    KineticsUnifiedSubset,
+    KineticsRootDirs,
+)
+
+from arn.scripts.research import arg_utils
+
+from exputils.io import create_filepath, parse_args
+
+
+def script_args(parser):
+    arg_utils.har_dataset_general(parser)
+    arg_utils.kinetics_root_dirs(parser)
+    arg_utils.single_label_config(parser)
+    arg_utils.single_kinetics_unified_subset(parser)
+
+def post_script_args(args):
+    return arg_utils.post_single_kinetics_unified_subset(
+        arg_utils.post_kinetics_root_dirs(args)
+    )
+
+def main():
+    args = post_script_args(parse_args(custom_args=script_args))
+
+    if not isinstance(args.bad_samples_dir, str):
+        raise ValueError('Must give this script `--bad_samples_dir` filepath.')
+
+    # Load the KineticsUnified dataset with the given KineticsUnifiedSubset and
+    # ensure that collect_bad_samples = True
+    kuni = KineticsUnified(
+        args.annotation_path,
+        args.kinetics_class_map_path,
+        args.kinetic_root_dirs,
+        args.subset,
+        collect_bad_samples=True,
+    )
+
+    #"""
+    dataloader = torch.utils.data.DataLoader(
+        kuni,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=12,
+        pin_memory=True,
+    )
+    #"""
+
+    # TODO use `get_path()` to check the K600 and K700 repeats of K400.
+
+    # Loop through the dataset's videos, it will save those that fail to load.
+    for vid in tqdm(enumerate(dataloader), total=len(dataloader)):
+        pass
+
+    # Save the corrupt samples, if any. Log if there are any or not.
+    kuni.data.iloc[[i.index for i in kuni.corrupt_videos]] \
+        .to_csv(create_filepath(os.path.join(
+            args.bad_samples_dir,
+            'corrupt_videos.csv',
+        )))
+
+    # Save the missing samples, if any. Log if there are any or not.
+    kuni.data.iloc[[i.index for i in kuni.missing_videos]] \
+        .to_csv(create_filepath(os.path.join(
+            args.bad_samples_dir,
+            'missing_videos.csv',
+        )))
+
+
+if __name__ == '__main__':
+    main()
