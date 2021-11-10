8b99c36f1c6d69de09c9de3b7346e8cb09a582f6
prijatelj <theomnipotentwoof@gmail.com>
2021-11-09 21:08:25 -0500

    WIP: init commit
    Former-commit-id: 939e3a16240433da2ba3a55da2fb67b744415b93 [formerly 44d035eef635000707db5c4ea2ed43be4a49f64e] [formerly 3a58317c7e982d07af37f61a4565684c894476f6] [formerly cc8f58a2740c06a9252a69e64703da41531140c5 [formerly cc8f58a2740c06a9252a69e64703da41531140c5 [formerly 7b0066419c44788a55ba8d21c51e3ae1dd0b35ff]]]
    Former-commit-id: 564ce77625ba1cd0c0d06e7f48f466038fb6c607 [formerly 6ed5059c17ede16a287d61e07ee7e6ba45624769]
    Former-commit-id: 0cede9fb874406155e8b7d14be0bf2ba86d3fe84
    Former-commit-id: 2654ea76454d5f7ee9fbc1c0763e9a08d14d7c86

---
+++ arn/data/par.py
@@ -0,0 +1,87 @@
+"""Dataset for the sample aligned Kinetics 400, 600, and 700_2020 datasets."""
+from collections import namedtuple
+from dataclasses import dataclass
+import os
+
+import pandas as pd
+
+from arn.data.kinetics_unified import (
+    KineticsUnified,
+    BadVideoSample,
+)
+
+
+@dataclass
+class PARData(KineticsUnified):
+    """The dataset for the PAR HAR data.
+
+    Attributes
+    ----------
+    data : pd.DataFrame
+        A DataFrame whose rows represents each sample's annotation data.
+    kinetics_class_map : pd.DataFrame
+        A mapping of the unique classes in each Kinetics dataset to one
+        another. May include other mappings as well. This serves the role of
+        older unique `class_labels`.
+    spatial_transform : torchvision.transforms.Compose = None
+        An image transformation that is applied to every video frame.
+    video_loader : callable = status_video_frame_loader
+        A callable that given a path loads the video frames from disk.
+    frame_step_size : int = 5
+        The step size used to select frames from the video to represent that
+        video in the sample. Named gamma tau in the X3D paper.
+    time_crops : int = 10
+        The total temporal crops of each video. This is specifically for use in
+        X3D.
+    randomize_spatial_params : bool = True
+        If True, randomizes the spatial transforms parameters.
+    sample_tuple : namedtuple
+        A namedtuple of the Kinetics Unified csv column names and serves to
+        contain a single sample (row) of the DataFrame with the loaded video
+        frames and annotations.
+    """
+    def __post_init__(
+        self,
+        annotation_path,
+        kinetics_class_map,
+        subset,
+        collect_bad_samples,
+    ):
+        if isinstance(kinetics_class_map, str):
+            ext = os.path.splitext(kinetics_class_map)[-1]
+            if ext == '.csv':
+                self.kinetics_class_map = pd.read_csv(kinetics_class_map)
+            elif ext == '.json':
+                self.kinetics_class_map = pd.read_json(kinetics_class_map)
+            else:
+                raise ValueError(' '.join([
+                    'Expected `kinetics_class_map` as a str to have',
+                    f'extention, ".csv" or ".json", not `{ext}`',
+                ]))
+        elif isinstance(kinetics_class_map, pd.DataFrame):
+            self.kinetics_class_map = kinetics_class_map
+        else:
+            raise TypeError(' '.join([
+                '`kinetics_class_map` expected to be str or pd.DataFrame, but',
+                f'recieved type: {type(kinetics_class_map)}',
+            ]))
+
+        self.data = pd.read_csv(annotation_path)
+
+        if 'video_path' not in self.data:
+            if self.video_dirs is None:
+                raise ValueError(' '.join([
+                    '`video_path` column must be in annotation data or',
+                    'video_dirs is given to generate the video paths.',
+                ]))
+            self.data['video_path'] = self.video_dirs \
+                + self.data['anonymous_id']
+
+        self.sample_tuple = namedtuple(
+            'par_data_sample',
+            ['video'] + self.data.columns.tolist(),
+        )
+
+        if collect_bad_samples:
+            self.corrupt_videos = []
+            self.missing_videos = []
