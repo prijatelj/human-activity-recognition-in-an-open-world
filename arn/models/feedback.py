058ba5b1c6edc517a9978dd59cd9ab1c38864ebd
prijatelj <theomnipotentwoof@gmail.com>
2021-11-11 11:07:47 -0500

    correct indenting, more docs/todos on structure
    Former-commit-id: d5a63d4520c01ba170c5a210b69697eff6a67926 [formerly cd1911f8176429af24c20fb20fe8b88fc4c44baf] [formerly 31bb1071e32f12ae13974ce56df1f2cebb203a6f] [formerly d6c3b00e262e0aaa89d12ca25b7bc11520757a1e [formerly d6c3b00e262e0aaa89d12ca25b7bc11520757a1e [formerly 62cd0cc321311ba6704480ec2e2cd26523e957fe]]]
    Former-commit-id: 7e1a2f4d5214244468ea3574252f81a9d803f175 [formerly 4d413f7acfb9a09078d1959be1b2bc409b681639]
    Former-commit-id: 2c278d75933469d91fb0f86290d16f0f51b70607
    Former-commit-id: ab4eb52650f24cf45c15162083c427c1e5064628

--- arn/models/feedback.py
+++ arn/models/feedback.py
@@ -7,26 +7,26 @@ import clip
 from exputils.data.labels import NominalDataEncoder
 
 
-class CLIPFeedback(object):
+class CLIPFeedbackInterpreter(object):
     """Feedback Interpretter using CLIP zeroshot.
 
     Attributes
     ----------
     clip : CLIP
         CLIP model used for encoding both text and images.
-    preprocess :
-        CLIP text preprocess.
     feedback_knowns : NominalDataEncoder
         Bidirectional map of feedback known texts to index. This indexing is
-        consistent across feedback_label_encs and feedback_similarity rows.
+        consistent across feedback_label_encs and similarity rows.
         This serves as the unique known feedback labels.
-    predictor_knowns : NominalDataEncoder
+    pred_knowns : NominalDataEncoder
         Bidirectional map of predictor known texts to index. This indexing is
-        consistent across predictor_label_encs and feedback_similarity columns.
+        consistent across predictor_label_encs and similarity columns.
         This serves as the unique known predictor labels.
     feedback_label_encs : torch.Tensor
-        CLIP pre-processed label encodings of PAR.
-    feedback_similarity : np.ndarray | torch.Tensor?
+        CLIP processed label encodings of feedback label text.
+    pred_label_encs : torch.Tensor
+        CLIP processed label encodings of predictor's known label text.
+    similarity : np.ndarray | torch.Tensor?
         A Matrix of rows being known feedback label texts and columns being the
         known predictor labels. This matrix's elements are the unnormalized
         cosine similarities of the feedback labels to predictor labels. This is
@@ -37,10 +37,32 @@ class CLIPFeedback(object):
     """
     def __init__(
         self,
-        model_path,
-        k600_label_encs,
-        par_label_encs,
+        clip_path,
+        pred_labels,
+        pred_label_encs=None,
+        feedback_labels=None,
+        feedback_label_encs=None,
+        similarity=None,
     ):
+        """Initialize and load the CLIPFeedbackInterpreter
+        Args
+        ----
+        clip_path : str
+            Path to the clip model
+        pred_labels : str | list(str) | NominalDataEncoder
+            Path to the predictor's known labels or the known labels themselves.
+        pred_label_encs : torch.Tensor = None,
+            Path to the predictor's known label encodings or the encoding
+            themselves.
+        feedback_labels : str | list(str) | NominalDataEncoder = None
+            Path to the known feedback labels or the labels themselves.
+        feedback_label_encs : torch.Tensor = None
+            Path to the known feedback label encodings or the encoding
+            themselves.
+        similarity : str | torch.Tensor =None
+            Path to the similarity martrix to be loaded or the similarity
+            matrix.
+        """
         # Load in CLIP Pre-trained for encoding new feedback label text
         self.clip, self.preprocess = clip.load(model_path, device)
 
@@ -52,6 +74,17 @@ class CLIPFeedback(object):
         # TODO label maps for feedback label text to idx, and predictor label
         # text to idx
 
+    def clip_enc_text(self, label_text):
+        """Return the clip encoding of the label text preserving shape.
+
+        label_text : list(list(str)) | np.ndarray(str)
+            A matrix of text strings where rows are samples and columns are the
+            number of classes given back sa feedback, which is assumed to be 5
+            due to protocol with PAR.
+        """
+
+        return
+
     def interpret_feedback(
         self,
         label_text,
@@ -61,57 +94,64 @@ class CLIPFeedback(object):
         preds=None
         predictor_belief=None
     ):
-    """Interpret the given feedback with the sample feature and task repr,
-    returning a soft label vector per sample for the knowns + prob of unknown.
+        """Interpret the given feedback with the sample feature and task repr,
+        returning a soft label vector per sample for the knowns + prob of
+        unknown.
 
-    Args
-    ----
-    label_text : list(list(str)) | np.ndarray(str)
-        A matrix of text strings where rows are samples and columns are the
-        number of classes given back sa feedback, which is assumed to be 5 due
-        to protocol with PAR.
-    feature_repr : torch.Tensor = None
-        The feature representation of the input samples.
-    task_repr : torch.Tensor = None
-        The fine-tuning or task representation of the samples.
-    videos : torch.Tensor
-        The actual input videos, aligned to the label text, feature_repr, and
-        task_repr, etc.
-    preds : torch.Tensor
-        The predictor's predictions for the sample.
-    pred_belief : torch.Tensor = None
-        The predictor's belief or certainty in the predictions for the sample.
+        Args
+        ----
+        label_text : list(list(str)) | np.ndarray(str)
+            A matrix of text strings where rows are samples and columns are the
+            number of classes given back sa feedback, which is assumed to be 5
+            due to protocol with PAR.
+        feature_repr : torch.Tensor = None
+            The feature representation of the input samples.
+        task_repr : torch.Tensor = None
+            The fine-tuning or task representation of the samples.
+        videos : torch.Tensor
+            The actual input videos, aligned to the label text, feature_repr,
+            and task_repr, etc.
+        preds : torch.Tensor
+            The predictor's predictions for the sample.
+        pred_belief : torch.Tensor = None
+            The predictor's belief or certainty in the predictions for the
+            sample.
 
-    Returns
-    -------
-    np.ndarray | torch.Tensor?
-        The probability vectors of predictor known classes and an unknown class
-        for each sample, which is a matrix of shape (samples, predictor_knowns
-        + 1). This indicates the predicted probablity that each feedback sample
-        corresponds to which predictor known class or none of them (the
-        unknown). Note that unknown probability is as the last element of each
-        vector row.
-    """
-    if isinstance(label_text, np.ndarray):
-        np.array(label_text)
+        Returns
+        -------
+        np.ndarray | torch.Tensor?
+            The probability vectors of predictor known classes and an unknown
+            class for each sample, which is a matrix of shape (samples,
+            predictor_knowns + 1). This indicates the predicted probablity that
+            each feedback sample corresponds to which predictor known class or
+            none of them (the unknown). Note that unknown probability is as the
+            last element of each vector row.
+        """
+        if isinstance(label_text, np.ndarray):
+            np.array(label_text)
 
-    # TODO Update running set of seen given labels. (OrderedDict keys)
-    new_feedback_labels = np.unique(label_text)
-    # TODO get the first occurrence index of the new feedback labels in
-    # label_text to save the resulting text encoding.
+        # TODO Update index encoding of known feedback labels.
+        new_feedback_labels = np.unique(label_text)
+        # TODO get the first occurrence index of the new feedback labels in
+        # label_text to save the resulting text encoding.
 
-    # TODO Sort each row of feedback labels lexically.
+        # TODO Sort each row of feedback labels lexically.
 
-    # TODO CLIP Encode the given text labels with preserved structure for
-    # getting indivudal feedback label encodings to be saved.
-    label_encs = text_zeroshot_encoding(self.clip, label_text, self.templates)
+        # TODO CLIP Encode the given text labels with preserved structure for
+        # getting indivudal feedback label encodings to be saved.
+        label_encs = text_zeroshot_encoding(
+            self.clip,
+            label_text,
+            self.templates,
+        )
 
-    # TODO ??? Obtain a CLIP encoding per row of feedback labels? Averaging?
+        # TODO ??? Obtain a CLIP encoding per row of feedback labels? Mean?
 
-    # TODO Get the cosine similaritis of each new feedback label to predictor's
-    # TODO Save these cosine similarities to the cosine similarity matrix
+        # TODO Get cosine similarity of each new feedback labels to predictor's
+        # TODO Save these cosine similarities to the cosine similarity matrix
 
-    # TODO Get the normalized cosine similarity to predictor's known labels
-    # TODO Get the probablity of none of the predictor's known labels (unknown)
+        # TODO Get the normalized cosine similarity to predictor's known labels
+        # TODO Get the probablity of none of the predictor's known labels
+        # (unknown)
 
-    return interpretted_feedback
+        return pred_known_or_unknown_probs
