2b803c5f981df5a39331e3b35b056e5355e5955d
prijatelj <theomnipotentwoof@gmail.com>
2021-11-10 20:26:35 -0500

    init feedback, figuring out exact black box design for us and Kitware/Par.
    Former-commit-id: 9d0d0008185f97cf80234d0358208a3ec1eaa693 [formerly 9d0d0008185f97cf80234d0358208a3ec1eaa693 [formerly a661832109dff63e03a8f3be65ef00c3d8dd1200]]
    Former-commit-id: 1732d56d14c0601d77f7f6bd18a5d2cc68c57e1b
    Former-commit-id: de997c5f1e1db721dacefeeb34719cdc319b1e38

---
+++ arn/models/feedback.py
@@ -0,0 +1,60 @@
+"""Feedback models, specifically CLIP label encoding."""
+import torch
+
+import clip
+
+
+class CLIPFeedback(object):
+    """Feedback Interpretter using CLIP zeroshot.
+
+    Attributes
+    ----------
+    clip : CLIP
+        CLIP model used for encoding both text and images.
+    preprocess :
+        CLIP text preprocess.
+    k600_label_encs : torch.Tensor
+        CLIP pre-processed label encodings of Kinetics600
+    par_label_encs : torch.Tensor
+        CLIP pre-processed label encodings of PAR.
+    """
+    def __init__(
+        self,
+        model_path,
+        k600_label_encs,
+        par_label_encs,
+    ):
+        # Load in CLIP Pre-trained
+        self.clip, self.preprocess = clip.load(model_path, device)
+
+
+    def interpret_feedback(
+        self,
+        class_text,
+        feature_repr=None,
+        task_repr=None,
+        videos=None,
+    ):
+    """Interpret the given feedback with the sample feature and task repr,
+    returning a soft label vector per sample for the knowns + prob of unknown.
+
+    Args
+    ----
+    class_text : list(list(str)) | np.ndarray(str) | pd.DataFrame
+        A matrix of text strings where rows are samples and columns are the
+        number of classes given back sa feedback, which is assumed to be 5 due
+        to protocol with PAR.
+    feature_repr : torch.Tensor = None
+        The feature representation of the input samples.
+    task_repr : torch.Tensor = None
+        The fine-tuning or task representation of the samples.
+    videos : torch.Tensor
+        The actual input videos, aligned to the class text, feature_repr, and
+        task_repr, etc.
+
+    Returns
+    -------
+    torch.Tensor
+    """
+
+    return
