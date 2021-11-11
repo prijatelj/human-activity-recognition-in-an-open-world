806a3893e5ad0b918adc23d60caf50a4ac750e4f
prijatelj <theomnipotentwoof@gmail.com>
2021-11-11 10:34:21 -0500

    WIP: reorg feedback attribs and filling in todos
    Former-commit-id: 60b27859df9c08e2ebc4454ef765d8ead2a9aeb7 [formerly d15e62218151374a1291d9c75bf783b692b3b87b] [formerly b793e0f8b066a52cecf3fd7632147d60bedc3644] [formerly 59c455a85013def04c4f90bbb1e4f00efdfdab0c [formerly 59c455a85013def04c4f90bbb1e4f00efdfdab0c [formerly de35530909a9b8336f904159cde91a9fb2d21679]]]
    Former-commit-id: 6eeedb177d707e21b67bc4746547b6b8fe23fbcb [formerly 1c6acac6dffafe3a585a5c1ccb3eb61c5aad8f56]
    Former-commit-id: ee7f29df957d371fd6550f42beec3b0745888dfe
    Former-commit-id: 3ae6c099a944ebf27d575fd7f9f14fb7c7974059

--- arn/models/feedback.py
+++ arn/models/feedback.py
@@ -1,7 +1,10 @@
 """Feedback models, specifically CLIP label encoding."""
+import numpy as np
 import torch
 
 import clip
+
+from exputils.data.labels import NominalDataEncoder
 
 
 class CLIPFeedback(object):
@@ -13,10 +16,20 @@ class CLIPFeedback(object):
         CLIP model used for encoding both text and images.
     preprocess :
         CLIP text preprocess.
-    k600_label_encs : torch.Tensor
-        CLIP pre-processed label encodings of Kinetics600
-    par_label_encs : torch.Tensor
+    feedback_knowns : NominalDataEncoder
+        Bidirectional map of feedback known texts to index. This indexing is
+        consistent across feedback_label_encs and feedback_similarity rows.
+        This serves as the unique known feedback labels.
+    predictor_knowns : NominalDataEncoder
+        Bidirectional map of predictor known texts to index. This indexing is
+        consistent across predictor_label_encs and feedback_similarity columns.
+        This serves as the unique known predictor labels.
+    feedback_label_encs : torch.Tensor
         CLIP pre-processed label encodings of PAR.
+    feedback_similarity : np.ndarray | torch.Tensor?
+        A Matrix of rows being known feedback label texts and columns being the
+        known predictor labels. The cosine similarity matrix is the cosine
+        similarity of these labels to
     """
     def __init__(
         self,
@@ -24,16 +37,20 @@ class CLIPFeedback(object):
         k600_label_encs,
         par_label_encs,
     ):
-        # Load in CLIP Pre-trained
+        # Load in CLIP Pre-trained for encoding new feedback label text
         self.clip, self.preprocess = clip.load(model_path, device)
 
-        # NOTE this may be the only part of the Predictor that does not need be
-        # a torch.nn.Module, but in the end
+        self.feedback_known = set()
 
+        # TODO Cosine similarity matrix of (feedback labels) X (predictor
+        # knowns) to one another when in CLIP encoded space.
+
+        # TODO label maps for feedback label text to idx, and predictor label
+        # text to idx
 
     def interpret_feedback(
         self,
-        class_text,
+        label_text,
         feature_repr=None,
         task_repr=None,
         videos=None,
@@ -43,7 +60,7 @@ class CLIPFeedback(object):
 
     Args
     ----
-    class_text : list(list(str)) | np.ndarray(str) | pd.DataFrame
+    label_text : list(list(str)) | np.ndarray(str)
         A matrix of text strings where rows are samples and columns are the
         number of classes given back sa feedback, which is assumed to be 5 due
         to protocol with PAR.
@@ -52,12 +69,29 @@ class CLIPFeedback(object):
     task_repr : torch.Tensor = None
         The fine-tuning or task representation of the samples.
     videos : torch.Tensor
-        The actual input videos, aligned to the class text, feature_repr, and
+        The actual input videos, aligned to the label text, feature_repr, and
         task_repr, etc.
 
     Returns
     -------
     torch.Tensor
     """
+    if isinstance(label_text, np.ndarray):
+        np.array(label_text)
 
-    return
+    # TODO Update running set of seen given labels. (OrderedDict keys)
+    new_feedback_labels = np.unique(label_text)
+    # TODO get the first occurrence index of the new feedback labels in
+    # label_text to save the resulting text encoding.
+
+    # TODO Sort each row of feedback labels lexically.
+
+    # TODO CLIP Encode the given text labels with preserved structure for
+    # getting indivudal feedback label encodings to be saved.
+    label_encs = text_zeroshot_encoding(self.clip, label_text, self.templates)
+
+    # TODO Obtain a CLIP encoding per row of feedback labels? Averaging?
+
+    # TODO Obtain a Cosine Similarity of each Feedback Label to
+
+    return interpretted_feedback
