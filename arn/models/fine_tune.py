5df4dedd511cdd07a968fcbecbb321da17d3aeb0
prijatelj <theomnipotentwoof@gmail.com>
2021-11-17 18:06:04 -0500

    updated fine_tune from sam
    Former-commit-id: 89a76ad5ab6ca272762592ad4882e3dbd41bee54 [formerly ef01cc906a81c96547a2adc10b29ee251e39d27f] [formerly 19f969b2fd3409499f2f6fb8a5b4a8b36ab64716] [formerly 2d89624a98076d176031afd41639c582e1e738dd [formerly 2d89624a98076d176031afd41639c582e1e738dd [formerly b3f94491a47574949ba2f471233b337087dcdbda]]]
    Former-commit-id: a226bc21b1e1880ce01ab031ef15933c68798580 [formerly be01d03138fa0cb604ac0cf633c8a8b8cc17de03]
    Former-commit-id: 48f49fc49de663215b86b92766b37424fcfd3422
    Former-commit-id: 60554e6be64fbadc7e4b61aa2bf506c528953a24

--- arn/models/fine_tune.py
+++ arn/models/fine_tune.py
@@ -1,7 +1,9 @@
 """Fine tuning models. These follow fine tuning in Pre-Trained Models"""
+import copy
 from collections import OrderedDict
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 # TODO Fine Tuning state: init, load, save
@@ -24,35 +26,100 @@ class FineTune(object):
         Though, batch size is handled by DataLoaders I believe. Let dataloaders
         handle what they can and their outputs be intput args. Everything will
         be torch.Tensors in and torch.Tensors out.
-
+    device : torch.device()
+        the device on which model should be trained
+        default: cpu
     eval_args : dict() = None
         The hyperparameters for evaluating the fine tuning model, if any.
         Often, there are none.
 
     [add anything else you deem necessary as state for the FineTune object.]
     """
-    def __init__(self, model, fit_args, eval_args=None):
+    def __init__(self, model, fit_args=None, device=torch.device("cpu"),  eval_args=None):
         if not isinstance(model, torch.nn.Module):
             raise TypeError(
                 'Expected model typed as `torch.nn.Module`, not {type(model)}'
             )
         self.model = model
+        self.device = device
+        if fit_args is None:
+            self.batch_size = 1000
+            self.epochs = 25
+        else:
+            self.batch_size = fit_args['batch_size']
+            self.epochs = fit_args['epochs']
 
         # TODO Save the fit args
 
         # TODO Save the eval/fwd pass args if any, probs not.
 
-        raise NotImplementedError()
+        # raise NotImplementedError()
 
-    def fit(self, features, labels):
+    def fit(self, features_t, labels_t, features_v, labels_v,  verbose=False):
         """Fits the model with fit_args and the given features and labels in a
         supervised learning fashion.
+        features_t labels_t: features and labels that the model should be trained on.
+        features_v labels_v: features and labels that the model should be validated on.
         """
+        t_len = len(features_t)
+        v_len = len(features_v)
+        print(t_len)
+        print(v_len)
+        # features_t = torch.stack(features_t)
+        # labels_t = torch.stack(labels_t)
+        # features_v = torch.stack(features_v)
+        # labels_v = torch.stack(labels_v)
 
-        # TODO perhaps not here, but should be easy to update the output size
-        # based on the labels.
-
-        raise NotImplementedError()
+        dataset = torch.utils.data.TensorDataset(features_t, labels_t)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
+        dataset_val = torch.utils.data.TensorDataset(features_v, labels_v)
+        dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=self.batch_size, shuffle=True)
+        model = self.model.to(self.device)
+        criterion = torch.nn.BCEWithLogitsLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
+        best_cls_loss = 99999999999999999
+        best_val_acc = 0
+        for epoch in range(self.epochs):
+            if verbose:
+                print("Epoch: " + str(epoch) + "---------------")
+            tot_cls_loss = 0.0
+            right = 0
+            for i, x in enumerate(dataloader):
+                torch.autograd.set_grad_enabled(True)
+                sfeatures, slabels = x
+                sfeatures = sfeatures.to(self.device)
+                slabels = slabels.to(self.device)
+                garbage, prediction = model(sfeatures)
+                right += torch.sum(
+                    torch.eq(torch.argmax(prediction, dim=1), torch.argmax(slabels, dim=1)).int()).cpu().numpy()
+                loss = criterion(prediction, slabels)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                tot_cls_loss += loss.item()
+            tacc = str(right / t_len)
+            right = 0
+            tot_cls_loss = 0.0
+            for i, x in enumerate(dataloader_val):
+                torch.autograd.set_grad_enabled(False)
+                sfeatures, slabels = x
+                sfeatures = sfeatures.cuda()
+                slabels = slabels.cuda()
+                garbage, prediction = model(sfeatures)
+                right += torch.sum(
+                    torch.eq(torch.argmax(prediction, dim=1), torch.argmax(slabels, dim=1)).int()).cpu().numpy()
+                loss = criterion(prediction, slabels)
+                tot_cls_loss += loss.item()
+            if best_cls_loss > tot_cls_loss:
+                best_cls_loss = tot_cls_loss
+                # torch.save(model.state_dict(), save_path + 'finetune_best.pt')
+                best_model = copy.deepcopy(model).cpu()
+                if verbose:
+                    print("New Best " + str(tot_cls_loss))
+                    print("Train Accuracy: " + tacc)
+                    print("Val Accuracy: " + str(right / v_len))
+        self.model = best_model
+        # return
 
     def extract(self, features):
         """Given features, outputs the fully connected encoding of them.
@@ -64,8 +131,6 @@ class FineTune(object):
         -------
         torch.Tensor
         """
-        raise NotImplementedError()
-
         return self.model.fcs(features)
 
     def predict(self, features, labels):
@@ -75,22 +140,26 @@ class FineTune(object):
 
         # NOTE for our paper, we want this with ability to find a threshold
         # from all train and val data.
-
-        raise NotImplementedError(
-            'Lowest priority. Mostly same as extract, just outputs class vecs.'
-        )
+        features, prediction = self.model(features)
+        return prediction
 
     def save(self, filepath):
-        # TODO Save the torch model, and the current parameter set (this class'
+        # this should work
         # args).
-        raise NotImplementedError()
 
-        torch.save(model, filepath)
+        torch.save(self.model, filepath)
 
         # TODO save args and other state info
 
     @staticmethod
-    def load(filepath, **kwargs):
+    def load(filepath,
+             input_size=400,
+             model_width=512,
+             n_layers=5,
+             out_features=512,
+             n_classes=29,
+             nonlinearity=nn.ReLU,
+             **kwargs):
         """Load all the pieces from a file(s), possibly be done thru a config
         file. For now, feel free to hard code expectation of FineTuneFCANN.
 
@@ -100,15 +169,15 @@ class FineTune(object):
             An instance of FineTune is returned given the files containing
             state information.
         """
-        raise NotImplementedError()
 
-        model = torch.load(filepath) # handles torch model when saved as .pt
-
+        state_dict = torch.load(filepath) # handles torch model when saved as .pt
+        model = FineTuneFCANN(input_size,model_width,n_layers,out_features,n_classes,nonlinearity)
+        model.load(state_dict)
         # TODO load the other parameters . . .
         #   If not loaded from file or whatever, then can simply be given by
         #   **kwargs.
 
-        return FineTune(model, )
+        return FineTune(model, **kwargs)
 
 
 class FineTuneFCANN(nn.Module):
