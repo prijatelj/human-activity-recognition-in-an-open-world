d25e798302b4b6c3555c1631a3a3f6781e75dc03
prijatelj <theomnipotentwoof@gmail.com>
2021-11-24 15:28:19 -0500

    corrected typo

--- arn/models/owhar.py
+++ arn/models/owhar.py
@@ -153,6 +153,6 @@ class OWHAPredictorEVM(object):
             input_samples = self.feature_repr.extract(input_samples)
 
         self.fine_tune.fit(input_samples, labels)
-        self.novelty_detect.fit(self.fine_tune.extract(input_samples), labels)
+        self.novelty_detector.fit(self.fine_tune.extract(input_samples), labels)
 
         # TODO update any other state for fititng, such as thresholds.
