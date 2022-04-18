d985c4bb09afa1c81e79a0208733f8daea4c660e
prijatelj <theomnipotentwoof@gmail.com>
2022-04-18 17:03:18 -0400

    WIP: towards initial start training and eval loop.

--- arn/data/kinetics_owl.py
+++ arn/data/kinetics_owl.py
@@ -9,6 +9,8 @@ Actuators: Feedback request system
     - Oracle feedback budgeted amount overall
     - Feedback Translation?
 """
+from typing import NamedTuple
+
 import torch
 
 from arn.data.kinetics_unified import KineticsUnified, KineticsUnifiedFeatures
@@ -18,17 +20,17 @@ from exputils.data.labels import NominalDataEncoder
 
 
 class DataSplits(NamedTuple):
-    """Contains the torch.utils.data.Dataset for train, validate, and test
+    """Contains the KineticsUnifiedFeatures for train, validate, and test
 
     Attributes
     ----------
-    train: torch.utils.data.Dataset = None
-    validate: torch.utils.data.Dataset = None
-    test: torch.utils.data.Datasetool = None
+    train: KineticsUnifiedFeatures = None
+    validate: KineticsUnifiedFeatures = None
+    test: KineticsUnifiedFeatures = None
     """
-    train: torch.utils.data.Dataset = None
-    validate: torch.utils.data.Dataset = None
-    test: torch.utils.data.Datasetool = None
+    train: KineticsUnifiedFeatures = None
+    validate: KineticsUnifiedFeatures = None
+    test: KineticsUnifiedFeatures = None
 
 
 class KineticsOWL(object):
@@ -45,6 +47,13 @@ class KineticsOWL(object):
         String identifier of a measure or a callable that takes as input
         (target, predictions) and returns a measurement or measurement
         object, e.g., confusion matrix.
+    eval_on_start : False
+        If False (the default), does not evaluate an untrained predictor. If
+        True, evaluated an untrained predictor. May be a good idea to evaluate
+        some untrained predictors, espcially if they were pre-trained.
+    experience : DataSplits = None
+        If `maintain_experience` is True in __init__, then the simulation
+        maintains the past experienced data for the predictor.
     """
     def __init__(
         self,
@@ -55,6 +64,8 @@ class KineticsOWL(object):
         rng_state=None,
         measures=None,
         #inc_splits_per_dset : 10
+        eval_on_start=False,
+        maintain_experience=False,
     ):
         """Initialize the KineticsOWL experiment.
 
@@ -70,6 +81,13 @@ class KineticsOWL(object):
         self.predictor = predictor
         self.feedback = feedback
         self.measures = measures
+        self.eval_on_start = eval_on_start
+
+        # TODO Maintain experience here for the predictor
+        if maintain_experience:
+            self.experience = DataSplits()
+        else:
+            self.experience = None
 
     @property
     def increment(self):
@@ -77,7 +95,43 @@ class KineticsOWL(object):
 
     def step(self, state):
         """The incremental step in incremental learning of Kinetics OWL."""
+        # 2. Inference/Eval on new data if self.eval_untrained_start
+        if self.increment == 0 and self.eval_on_start:
+            # 1. Get new data (input samples only)
+            new_data_splits = self.environment.step()
+
+            # TODO Novelty Detection for the Task
+
+            # TODO Predict for the Task
+            pred = self.predictor.predict(new_data_splits) # TODO data pass!
+
+            # TODO Optional log/save predictions or eval measures
+            self.eval(new_data_splits, pred)
+
+        if self.feedback == 'oracle':
+            # 3. Opt. Feedback on new data
+            new_data_splits = self.environment.feedback(new_data_splits)
+
+            if self.experience:
+                # TODO Add new data to experience
+                raise NotImplementedError('Added new data to experience')
+
+            # TODO 4. Opt. Predictor Update/train on new data w/ feedback
+            self.predictor.fit(self.experience)
+
+            # TODO 5. Opt. Predictor eval post update
+            self.eval(new_data_splits, pred)
+
+            # TODO 6. Opt. Evaluate the updated predictor on entire experience
+            #self.eval(self.experience, self.predictor.predict(self.experience))
+
+            raise NotImplementedError()
+
+    def eval(self, dataset, pred):
+        # TODO evaluate the given dataset on all measures.
         raise NotImplementedError()
+
+        # TODO record running measures across entire experience as well.
 
     def run(self, max_steps=None, tqdm=None):
         """The entire experiment run loop."""
@@ -95,6 +149,7 @@ class KineticsOWL(object):
                 if self.predictor.feedback_query():
                     raise NotImplementedError()
 
+
 class KineticsOWLExperiment(object):
     """The Dataloading and handling for a Kinetics OWL experiment.
 
@@ -111,7 +166,7 @@ class KineticsOWLExperiment(object):
         The starting increment's data as a KineticsUnifiedFeatures object.
 
         huh... docstr does not CAP gen on MultiType ... | KineticsUnified
-    step : list = None
+    steps : list = None
         List of DataSplits containing KineticsUnifiedFeature objects
         representing the order to increment over them.
 
@@ -123,13 +178,13 @@ class KineticsOWLExperiment(object):
     _increment : int = 0
         The current increment of the experiment. Starts at zero, increments
         after a step is complete. After initial increment is increment = 1.
-    label_encoder : exputils.data.labels import NominalDataEncoder
+    label_encoder : exputils.data.labels.NominalDataEncoder
         Keep the labels consistent at the current step.
     """
     def __init__(
         self,
         start,
-        step=None,
+        steps=None,
         inc_splits_per_dset=10,
         #feature_extract=True,
         #shared_dataloader_kwargs=None,
@@ -141,14 +196,15 @@ class KineticsOWLExperiment(object):
         Args
         ----
         start : see self
-        step : see self
+        steps : see self
         inc_splits_per_dset : see self _inc_splits_per_dset
         seed : int = None
             The seed for the random number generator
         """
+        self._increment = 0
         self._inc_splits_per_dset = inc_splits_per_dset
         self.start = start
-        self.step = step
+        self.steps = steps
 
         # TODO need to create a dataset from the provided start and step
         # datasets.
@@ -167,22 +223,41 @@ class KineticsOWLExperiment(object):
     @property
     def increment(self):
         """The current increment or steps taken."""
-        self.environment._increment
+        return self._increment
 
     @property
     def increments_per_dataset(self):
         return self._inc_splits_per_dset
+
+    @property
+    def total_increments(self):
+        """Start increment + steps * increments per dataset in steps"""
+        return 1 + len(self.steps) * self.increments_per_dataset
+
+    def feedback(self, data_splits):
+        # TODO Oracle, exhaustive, no budget : labels are simply provided.
+        if data_splits.train and not data_splits.train.return_label:
+            data_splits.train.return_label = True
+        if data_splits.validate and not data_splits.validate.return_label:
+            data_splits.validate.return_label = True
+        if data_splits.test and not data_splits.test.return_label:
+            data_splits.test.return_label = True
+        return data_splits
 
     def step(self):
         """An incremental step's train, val, and test dataloaders?
 
         Returns
         -------
-        (object, object=None)
-            A tuple of a Torch dataloader object for the specified increment's
-            new data, and an optional other dataloader for the history of data
-            the predictor recalls. The latter being a convenience where it is
-            the predictor's stored experience object
+        DataSplits
+            A NamedTuple of a Torch Dataset objects for the current increment's
+            new data
         """
-        raise NotImplementedError()
         # TODO Manage the location of data and tensors to avoid memory issues.
+        if self.increment == 0:
+            self._increment += 1
+            return self.start
+        if self.increment >= self.total_increments:
+            raise ValueError('Experiment Complete: step datasets exhausted.')
+
+        raise NotImplementedError('Stepping increments through step datasets.')
