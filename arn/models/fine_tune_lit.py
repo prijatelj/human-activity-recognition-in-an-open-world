95254a83a3ebf7e459fc490bfd46e8fa6825f293
prijatelj <theomnipotentwoof@gmail.com>
2022-04-19 16:07:17 -0400

    added init_trainer docstr prototype workaround, updated the docs w/ copy/paste cuz again docstr prototype workaround, added model eval at extract time, may be wrong.

--- arn/models/fine_tune_lit.py
+++ arn/models/fine_tune_lit.py
@@ -1,4 +1,4 @@
-"""FineTune but in Pytorch Lightning for simplicty."""
+"""FineTune written in Pytorch Lightning for simplicty."""
 import pytorch_lightning as pl
 import torch
 nn = torch.nn
@@ -7,19 +7,79 @@ from arn.models.fine_tune import FineTuneFC
 from arn.torch_utils import torch_dtype
 
 
+def init_trainer(
+    default_root_dir=None,
+    enable_checkpointing=True,
+):
+    """Hotfix docstr workaround for not being able to read Torch docs and not
+    being able to accept/parse uknown kwargs to be passes as **kwargs.
+
+    Args
+    ----
+    default_root_dir : str = None
+    enable_checkpointing : bool = True
+
+    Returns
+    -------
+    pytorch_lightning.Trainer
+        docstr TODO check the module's namespace to support `as pl` and then
+        pl.Trainer in docs.
+    """
+    return pl.Trainer(
+        default_root_dir=default_root_dir,
+        enable_checkpointing=enable_checkpointing,
+    )
+
+
 class FineTuneFCLit(FineTuneFC, pl.LightningModule):
     """The FineTune model exteneded as a pl.LightningModule
 
     Attributes
     ----------
-    see FineTuneFC
+    fcs : torch.nn.Sequential
+        The fully connected sequential dense ANN whose output is the
+        penultamite layer of the network just before the linear, softmaxed
+        layer.
+    classifier : torch.nn.Linear
+        The remaining portion of the sequential model that takes as input
+        the output of fcs and maps that to a dense layer. Note that this does
+        not apply log_softmax to the output of the linear, only puts it into
+        the correct number of dimensions (number of classes). The log_softmax
+        is applied to the first dimension in the `forward()` method.
+
+
+    TODO
+    ----
+    todo `see FineTuneFC`
+        In docstr, add see across classes/objects in general, not just in
+        functions for self.
     """
-    def __init__(self, *args, loss=None, **kwargs):
+    def __init__(self, loss=None, *args, **kwargs):
         """Initialize the FineTune model
 
         Args
         ----
-        see FineTuneFC
+        loss : torch.nn.modules.loss._Loss = None
+            TODO in docstr add support for `see FineTuneFC.__init__`
+        input_size : int
+            The input size of the input linear layer.
+        width : int = 512
+            The width of the hidden layers within the ANN.
+        depth : int = 5
+            The depth or number of hidden layers within the ANN.
+        feature_repr_width : int = None
+            The width of the penultamite layer of this ANN, which is the layer
+            just before the output(softmax) and serves as the feature
+            representation of the ANN. By default, this is None and not set,
+            which means the feature representation layer will use the same
+            `width` as the other hidden layers.
+        n_classes : int = 29
+            The number of classes to expect for the output layer of this ANN.
+        activation : torch.nn.Module = torch.nn.LeakyReLU
+            The activation to apply after every linaer layer.
+        dropout_prob : float = None
+            The probability for the dropout layers after the linear layers.
+            Defaults to None, meaning no dropout is applied.
         """
         super().__init__(*args, **kwargs)
 
@@ -65,6 +125,8 @@ class FineTuneLit():
         docstr needs to support subclasses of a given class: torch.nn.Module
         The model to be used for fine tuning. This is expected to support
         FineTuneFC.
+    trainer : init_trainer
+        The pl.Trainer trainer used for the fine tune model.
     batch_size : int = 1000
     epochs : int = 25
         Number of epochs to use during fitting.
@@ -78,13 +140,14 @@ class FineTuneLit():
     def __init__(
         self,
         model,
+        trainer,
         batch_size=1000,
         epochs=25,
         device='cpu',
         dtype=torch.float32,
         shuffle=True,
-        *args,
-        **kwargs,
+        #*args,
+        #**kwargs,
     ):
         """Init the FineTune model.
 
@@ -104,7 +167,8 @@ class FineTuneLit():
         self.device = torch.device(device)
         self.dtype = torch_dtype(dtype)
 
-        self.trainer = pl.Trainer(*args, **kwargs)
+        #self.trainer = pl.Trainer(*args, **kwargs)
+        self.trainer = trainer
 
     def fit(self, dataset, val_dataset=None):
         train_loader = torch.utils.data.DataLoader(
@@ -132,4 +196,5 @@ class FineTuneLit():
         return self.model(features.to(self.device, self.dtype))[1]
 
     def extract(self, features):
+        self.model.eval()
         return self.model(features.to(self.device, self.dtype))[0]
