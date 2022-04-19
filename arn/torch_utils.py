c87dfaa3ce30f979c7ba2ba8cc3c1b2974ba845f
prijatelj <theomnipotentwoof@gmail.com>
2022-04-19 00:35:15 -0400

    corrected to return the dtype

--- arn/torch_utils.py
+++ arn/torch_utils.py
@@ -4,11 +4,11 @@ import torch
 def torch_dtype(dtype):
     """Given dtype as str or torch dtype, perform the casting, and checks."""
     if isinstance(dtype, torch.dtype):
-        self.dtype = dtype
+        return dtype
     elif isinstance(dtype, str):
         dtype = getattr(torch, dtype, None)
         if isinstance(dtype, torch.dtype):
-            self.dtype = dtype
+            return dtype
         else:
             raise TypeError('Expected torch.dtype for dtype not: {dtype}')
     else:
