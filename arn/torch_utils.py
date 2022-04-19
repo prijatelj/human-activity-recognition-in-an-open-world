9c3a0a9ff906c39161025fff739e8c4b0783c5a8
prijatelj <theomnipotentwoof@gmail.com>
2022-04-19 16:05:36 -0400

    added support for None pass through.

--- arn/torch_utils.py
+++ arn/torch_utils.py
@@ -3,7 +3,9 @@ import torch
 
 def torch_dtype(dtype):
     """Given dtype as str or torch dtype, perform the casting, and checks."""
-    if isinstance(dtype, torch.dtype):
+    if dtype is None:
+        return None
+    elif isinstance(dtype, torch.dtype):
         return dtype
     elif isinstance(dtype, str):
         dtype = getattr(torch, dtype, None)
