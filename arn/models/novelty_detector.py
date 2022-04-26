8f405b002f2e0917117b590aa922377afc6e1b2e
prijatelj <theomnipotentwoof@gmail.com>
2022-04-26 18:36:01 -0400

    WIP: added novelty detector placeholder for hypothesis test or bayes factor of KLDiv of train to the moving windows.

--- arn/models/novelty_detector.py
+++ arn/models/novelty_detector.py
@@ -12,22 +12,159 @@ import torch
 #    def detect(self, known_probs):
 #        pass
 
+class WindowedKLDivBayesFactor(object):
+    """A Novelty Detector over time where samples are timesteps. This uses the
+    predictor's output along with a distribution check of the KLdivergences
+    from train to the pre-novelty to the train to the out of pre-train novelty.
+
+    Attributes
+    ----------
+    window_size : int
+        The number of samples to be included within the sliding window.
+    detection_threshold : float = 0.5
+        The chosen threshold for determining when a probability of novelty is
+        large enough to be deemed a detection of novelty.
+    distrib : torch.distributions.Distribution | tuple = None
+        Distribution used to determine the KLDivergence between the train data
+        and the window based on the parameters of that distribution found from
+        the data. When given one torch.distributions.Distribution the same
+        distirbution is used for train data, pre-novelty data, and test data,
+        which assumes they are all of the same family of distributions. This
+        Defaults to torch.distributions.Normal.
+
+        Perhaps, if given 2 distirbution objects, could separate train distrib
+        to window distrib (same for both pre-novelty and at eval time).
+
+        Given this is at root for multiclass classificaiton, the distrib could
+        be a Dirichelt. If only binary known vs unknown, then could be
+        Binomial. Otherwise, when treated as regression w/ some threshold then
+        is a linear regression, thus Gaussian makes sense, because of its
+        relation to Mean Squared Error.
+    sliding_window : list
+        The list of samples that serves as the sliding window.
+    has_world_changed : bool = False
+        False if the world has not been deemed to have changed given prior
+        observations, otherwise True.
+
+    NOTE
+    ----
+    All this seems too hacky. Just do proper out-of-distribution detection with
+    something like MCMC or a [C]VAE. Set the proper convergence criterion and
+    let compute go as you can.
+    """
+    def __init__(
+        self,
+        window_size=200,
+        detection_threshold=0.5,
+        distrib=None
+    ):
+        self.window_size = window_size
+        self.detection_threshold = detection_threshold
+        self.has_world_changed = False
+        self.sliding_window = []
+
+        if distrib is None:
+            self.distrib = torch.distributions.Normal
+        self.in_sample_train = None
+        self.in_sample_window = None
+        self.out_sample_window = None
+
+    def fit(self, train_pred, pre_novelty_pred, distrib=None):
+        """Fit the train distrib.
+
+        Args
+        ----
+        train_pred : torch.Tensor
+        in_sample_windows : list
+        """
+        raise NotImplementedError()
+        # TODO: fit the train data distrib, fit the in-sample window distrib
+        self.in_sample_train = self.distrib()
+        self.in_sample_window = self.distrib()
+
+        self.in_sample_kl = torch.distributions.kl.kl_divergence(
+            self.in_sample_train,
+            self.in_sample_window
+        )
+
+        # This needs to fit the distrib of in-sample KL over the windows of
+        # pre-novelty...
+
+        # TODO should just fit some distrib to the train max-preds, the
+        # in-sample windows' preds, and determine how out of distribution the
+        # novel data is: Bayes Factor.
+
+        # From these distribs determine the KLDiv of in sample window to train
+        # (difference in in sample window to train).
+
+        # From that, that establishes the minimum error/divergece to expect and
+        # tolerate overtime in eval.
+
+        # Then set a threshold based on validation data to determine how.
+
+    def detect(
+        self,
+        known_probs,
+        is_max_pred=False,
+        distrib=None,
+        logger=None,
+    ):
+        """Post probability score classification, perform detection using KL
+        Divergence with a weighted threshold.
+
+        Args
+        ----
+        known_probs : torch.Tensor
+            Something confusing involving EVMWindowedMeanKLDiv.predict(input_samples).
+            This is the probability vectors per sample of known classes.
+        is_max_known_probs : bool = False
+            If True, known_probs is the maximum probability per sample's predicted
+            probability vector may be provided if precalculated.
+        logger : logging
+            A logging object to preserve the functionality up-stream.
+
+        Returns
+        -------
+        torch.Tensor
+            A vector of "probability" values [0, 1] indicating if novelty is to
+            be detected at the current sample given all prior samples and prior
+            runs captured in this objects state information, e.g. the KL
+            Divergence threshold after decay.
+        """
+        raise NotImplementedError()
+        self.out_sample_window = self.distrib()
+        self.out_sample_kl = torch.distributions.kl.kl_divergence(
+            self.in_sample_train,
+            self.in_sample_window
+        )
+        #return detections?
+
+
 class WindowedMeanKLDiv(object):
     """A Novelty Detector consisting of the EVM and weighted KL Divergence.
     This method relies on a moving average using a sliding window.
 
     Attributes
     ----------
-    detection_threshold : float
+    detection_threshold : float = 0.5
+        The chosen threshold for determining when a probability of novelty is
+        large enough to be deemed a detection of novelty.
     has_world_changed : bool = False
+        False if the world has not been deemed to have changed given prior
+        observations, otherwise True.
     accuracy : float
     kl_threshold : float
     kl_threshold_decay : float
     mean_train : float
     std_dev_train : float
     window_size : int
+        The number of samples to be included within the sliding window.
     sliding_window : list
+        The list of samples that serves as the sliding window.
     past_window : list
+        An extra window to use in computation. Why? idk, not my code (DSP).
+        Could just double the sliding window size given the current
+        implementation.
 
     Note
     ----
