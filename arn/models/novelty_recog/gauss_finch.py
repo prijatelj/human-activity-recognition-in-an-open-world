a4fc8c272a08507f11488cecba894584bfa3181d
prijatelj <derek.s.prijatelj@gmail.com>
2022-09-30 19:58:25 -0400

    moved gaussian detect and recog into gauss FINCH

--- arn/models/novelty_recog/gauss_finch.py
+++ arn/models/novelty_recog/gauss_finch.py
@@ -59,10 +59,11 @@ class GaussFINCH(GaussianRecognizer):
         if self.gmm is not None:
             return self.gmm.label_enc
 
-    def fit_knowns(self, features, val_dataset=None):
+    def fit_knowns(self, features, labels, val_dataset=None):
         # TODO update known label_enc using GMM.fit() or gmm_fit()
         if self.known_gmm is None:
             self.known_gmm = GMM(
+                NominalDataEncoder(np.unique(dataset.labels)) # TODO ???
                 min_samples=0,
             )
         self.known_gmm.fit(features, known_labels)
@@ -120,3 +121,81 @@ class GaussFINCH(GaussianRecognizer):
         )
 
         self.gmm = join_gmms(self.known_gmm, self.unknown_gmm)
+
+    def recognize(self, features, detect=False):
+        """Using the existing Gaussians per class-cluster, get log probs."""
+        # Normalize the probability each feature belongs to a recognized class,
+        # st the recognized classes are mutually exclusive to one another.
+        recogs = torch.stack(
+            [mvn.log_prob(features) for mvn in self._gaussians],
+            dim=1,
+        )
+        if detect:
+            thresholds = torch.Tensor(self._thresholds)
+            detect_unknowns = (recogs < thresholds).all(1)
+
+            recogs = F.pad(F.softmax(recogs, dim=1), (1, 0), 'constant', 0)
+
+            # Sets unknown to max prob value, scales the rest by 1 - max
+            if detect_unknowns.any():
+                recogs[detect_unknowns, 0] = \
+                    recogs[detect_unknowns].max(1).values
+                recogs[detect_unknowns, 1:] *= 1 \
+                    - recogs[detect_unknowns, 0].reshape(-1, 1)
+            return recogs
+
+        return F.softmax(recogs, dim=1)
+
+    def detect(self, features, knowns_only=True):
+        """Given data samples, detect novel samples to the known classes.
+
+        fit/recog: If given unlabeled data, decide if any of those samples form
+        new clusters that consitute as a new class based on the initial
+        hyperparameters.
+
+        Recognized novel unknown classes are represented as 'unknown_#' where #
+        is the newest class number.
+
+        Criteria for a new class cluster:
+        - min number of samples
+        - which density param is acceptable?
+            min density of any class XOR density level of nearest class.
+
+        Args
+        ----
+        features : torch.Tensor
+            2 dimensional float tensor of shape (samples, feature_repr_dims).
+        preds : torch.Tensor
+            1 dimensional integer tensor of shape (samples,). Contains the
+            index encoding of each label predicted per feature.
+        n_expected_classes : int = None
+            The number of expected classes, thus including the total known
+            classes + unknown class + any recognized unknown classes.
+
+        Returns
+        -------
+        torch.Tensor
+            The predicted class label whose values are within label_enc.
+        """
+        if not self._gaussians:
+            raise ValueError('Recognizer is not fit: self._gaussians is None.')
+
+        # NOTE IDEAL elsewhere: Detect is an inference time only task, and
+        # inherently binary classificicaiton of known vs unknown.
+        #   0. new data is already assumed to have been used to fit ANN / recog
+        #   1. call `class_log_probs = recognize(features)`
+        #   2. reduce unknowns.
+        #   3. return sum of probs of unknowns, that is detection given recog.
+        # NOTE in this method, there is disjoint between ANN and recog.
+        #   with this approach, perhaps should compare just DPGMM on frepr to
+        #   DPGMM on ANN of frepr.
+        # Given never letting go of all experienced data, the DPGMM on ANN
+        #   has a better chance of success over the increments than on just the
+        #   frepr as the frepr currently is frozen over the increments.
+
+        num_labels = self.n_known_labels if knowns_only else self.n_labels
+        thresholds = torch.Tensor(self._thresholds[:num_labels])
+        return (torch.stack(
+            [mvn.log_prob(features) for mvn in self._gaussians[:num_labels]],
+            dim=1,
+        ) < thresholds).all(1)
