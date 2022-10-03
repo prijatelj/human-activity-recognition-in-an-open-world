538692ead65e51a079009c43dacb65ddbfb6018b
prijatelj <derek.s.prijatelj@gmail.com>
2022-10-03 13:53:44 -0400

    WIP: filled in a lot of methods and use case, remaining is fit_knowns.

--- arn/models/novelty_recog/gmm_finch.py
+++ arn/models/novelty_recog/gmm_finch.py
@@ -25,64 +25,89 @@ class GMMFINCH(GMMRecognizer):
 
     Attributes
     ----------
-    gmms : list(GMM)
+    known_gmms : list(GMM) = None
         List of Gaussian Mixture Model objects per class, where classes consist
-        of knowns and unknown.
-    label_enc : NominalDataEncoder
-        The label encoder corresponding to the classes with GMMs. This follows
-        the order of unknown (index 0), then knowns.
+        of knowns.
+    see GMMRecognizer.__init__
     """
-    def __init__(self, *kwargs):
-        raise NotImplementedError
-
-        # TODO self.known_label_enc...
-        # TODO self._label_enc...
+    def __init__(self, *args, *kwargs):
+        """
+        Args
+        ----
+        see GMMRecognizer.__init__
+        """
+        # NOTE uses self.known_label_enc and self._label_enc
+        super().__init__(*args, **kwargs)
+        self.known_gmms = None
 
     def add_new_knowns(self, new_knowns):
-        raise NotImplementedError
+        raise NotImplementedError('Anything different from parent?')
 
     def reset_recogs(self):
         """Resets the recognized unknown class-clusters, and label_enc"""
-        raise NotImplementedError
         super().reset_recogs()
-        # TODO Reset general recognizer to use just knowns
 
-    def fit_knowns(self, features):
+        # Reset general recognizer to use just knowns
+        self._label_enc = deepcopy(self.known_label_enc)
+
+    def fit_knowns(self, features, labels, val_dataset=None):
         raise NotImplementedError
+
+        # TODO for each known class with labels, fit the GMM.
 
     def recognize_fit(self, features, n_expected_classes=None, **kwargs):
-        raise NotImplementedError
+        if not self.known_gmms:
+            raise ValueError('Recognizer is not fit: self.known_gmms is None.')
 
-        # NOTE Given multiple GMMs, find the thresholds per GMM (i don't think
-        # this applies, just use detect() of the GMM.)
+        super().recognize_fit(features, n_expected_classes, **kwargs)
 
-        # NOTE return the list of GMMs, each w/ thier own label_enc, gaussians,
-        # and thresholds and methods for finding recog, detect w/in, and
-        # returning the log prob of samples belonging to the entire GMM
+        # Update the general knowns + unknown recogs expanded
+        self._label_enc = deepcopy(self.known_label_enc)
 
     def recognize(self, features, detect=False):
-        raise NotImplementedError
+        # Loop through all known gmms + unknown_recogs getting log_probs.
+        recogs = [gmm.log_prob(features) for gmm in self.known_gmms]
+
+        if self.has_recogs:
+            unknown_log_probs = self.unknown_gmm.comp_log_prob(features)
+            recogs = torch.cat([recogs, uknown_log_probs], dim=1)
+
+        if detect:
+            detect_unknowns = (recogs < self.thresholds).all(1)
+
+            recogs = F.pad(F.softmax(recogs, dim=1), (1, 0), 'constant', 0)
+
+            # Sets unknown to max prob value, scales the rest by 1 - max
+            if detect_unknowns.any():
+                recogs[detect_unknowns, 0] = \
+                    recogs[detect_unknowns].max(1).values
+                recogs[detect_unknowns, 1:] *= 1 \
+                    - recogs[detect_unknowns, 0].reshape(-1, 1)
+            return recogs
+        return F.softmax(known_log_probs, dim=1)
 
     def detect(self, features, known_only=True):
-        raise NotImplementedError
+        recogs = [gmm.log_prob(features) for gmm in self.known_gmms]
+
+        if self.has_recogs:
+            unknown_log_probs = self.unknown_gmm.comp_log_prob(features)
+            recogs = torch.cat([recogs, uknown_log_probs], dim=1)
+
+        return (recogs < self.thresholds).all(1)
 
     def save(self, h5, overwrite=False):
-        raise NotImplementedError
         close = isinstance(h5, str)
         if close:
             h5 = h5py.File(create_filepath(h5, overwrite), 'w')
 
-        # Save the attrs unique to this object
-        h5.attrs['level'] = self.level
-
-        # Save known_gmm, unknown_gmm, but NOT gmm, as it is joined by the 2.
-        if self.known_gmm:
-            self.known_gmm.save(h5.create_group('known_gmm'))
+        # Save known_gmms, but NOT gmm, as it is joined by the 2.
+        knowns = h5.create_group('known_gmms')
+        for gmm in self.known_gmms:
+            gmm.save(knowns.create_group(gmm.unknown_key))
 
         super().save(h5)
         if close:
             h5.close()
-
 
     @staticmethod
     def load(h5):
