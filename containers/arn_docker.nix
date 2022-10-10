b4b4a0172ff7a7e55cf9d46b165f6c2dab89c38b
prijatelj <derek.s.prijatelj@gmail.com>
2022-10-09 22:35:18 -0400

    WIP: still figuring out nix with python, torch, and opencv

--- containers/arn_docker.nix
+++ containers/arn_docker.nix
@@ -1,27 +1,73 @@
 # Build a Docker image using Nix and dockerTools.
-{ pkgs ? import <nixpkgs> {} 
+{ pkgs ? import <nixpkgs> { } 
 , pkgsLinux ? import <nixpkgs> { system = "x86_64-linux"; }
 }:
 
-let peanut = pkgs.dockerTools.pullImage {
+let
+/*
+  peanut = pkgs.dockerTools.pullImage {
   imageName = "nvcr.io/nvidia/pytorch";
   imageDigest = "sha256:ab303284646e417327ae1589b3768e04211669438110efec6c7fccad476b4b18";
   sha256 = "053pf4y23aa821sj4mc5lkl6nxs9vg40rr0k7bq3snlyryw9igsl";
   finalImageName = "nvcr.io/nvidia/pytorch";
   finalImageTag = "21.09-py3";
 };
-in pkgs.dockerTools.buildImage {
+  #python-with-pkgs = python3.withPackages python-pkgs;
+  python_pkgs = with pkgs.python39Packages; [
+    numpy
+    scipy
+    scikit-learn
+    pandas
+    h5py
+    tqdm
+    torchWithCuda
+    torchvision
+    #torchsummary
+    torchmetrics
+    pytorch-lightning
+    pyro-ppl
+    cython
+    hdbscan
+    opencv4
+    pyaml
+    #(opencv4.override { enableGtk2 = true; })
+  ]; 
+#*/
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix";
+    ref = "refs/tags/3.5.0";
+  }) {};
+in
+pkgs.dockerTools.buildImage {
     name = "arn";
     tag = "0.2.0rc1";
 
-    fromImage = peanut;
+    #fromImage = peanut;
     #fromImageName = "74d53f84c686";
     #fromImageTag = "21.09-py3";
+
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      #pathsToLink= [ "/bin" ];
+      paths = concatLists [
+        mach-nix.mkNixpkgs {
+          requirements = "./requirements/arn.txt"
+          packagesExtra = [
+            docstr
+          ]
+        }
+        [
+          pkgs.git
+          pkgs.curl
+          pkgs.cudatoolkit
+          pkgs.stdenv.cc
+        ]
+    };
 
     runAsRoot = ''
         #!${pkgs.runtimeShell}
         # Install this repo's main package and its dependencies
-        pip install -e . -r requirements/arn_dev.txt
+        #pip install -e . -r requirements/arn_dev.txt
 
         # Install docstr for the cli of the package
         pip install docstr==0.0.3rc2
