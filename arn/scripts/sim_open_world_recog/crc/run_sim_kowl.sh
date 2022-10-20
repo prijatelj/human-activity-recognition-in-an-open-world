12f044fcbde7bac9bbdbe570db12af8f5c627151
prijatelj <theomnipotentwoof@gmail.com>
2022-10-20 13:50:30 -0400

    corrected data paths.

--- arn/scripts/sim_open_world_recog/crc/run_sim_kowl.sh
+++ arn/scripts/sim_open_world_recog/crc/run_sim_kowl.sh
@@ -8,8 +8,11 @@
 # respective expected paths within the used config
 CODE_PATH="/scratch365/dprijate/osr/har"
 DATA_PATH="/afs/crc.nd.edu/group/cvrl/scratch_21/dprijate/har"
+#CODE_PATH="$DATA_PATH/"
 
 FEEDBACK_AMOUNT="1.0"
+
+#docstr arn/arn/scripts/sim_open_world_recog/configs/exp2_gmm_finch.yaml \
 
 apptainer exec \
     --nv \
@@ -17,5 +20,10 @@ apptainer exec \
     --bind $DATA_PATH:/tmp/har/data \
     --pwd /tmp/har/code \
     $CODE_PATH/data/containers/arn_latest.sif \
-    docstr arn/arn/scripts/sim_open_world_recog/configs/exp2_gmm_finch.yaml \
-        --feedback_amount $FEEDBACK_AMOUNT
+    docstr arn/arn/scripts/sim_open_world_recog/configs/container_test-run_exp2_2d_sim.yaml \
+        --predictor.load_inc_paths "$DATA_PATH/"
+        #--feedback_amount $FEEDBACK_AMOUNT \
+
+
+#python -c \
+#        'import torch; print(torch.__version__); print(torch.cuda.is_available())'
