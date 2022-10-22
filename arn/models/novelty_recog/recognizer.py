afb48207d30a7e0c14b39c730628a6b6a5b93303
prijatelj <theomnipotentwoof@gmail.com>
2022-10-22 00:52:36 -0400

    corrected call to static method

--- arn/models/novelty_recog/recognizer.py
+++ arn/models/novelty_recog/recognizer.py
@@ -75,9 +75,13 @@ def load_owhar(h5, class_type=None):
         ).convert_dtypes([int, str, str, bool])
 
     if '_known_label_enc' in h5:
-        loaded._known_label_enc.load_h5(h5['_known_label_enc'])
+        loaded._known_label_enc = NominalDataEncoder.load_h5(
+            h5['_known_label_enc']
+        )
     if '_recog_label_enc' in h5:
-        loaded._recog_label_enc.load_h5(h5['_recog_label_enc'])
+        loaded._recog_label_enc = NominalDataEncoder.load_h5(
+            h5['_recog_label_enc']
+        )
 
     if close:
         h5.close()
