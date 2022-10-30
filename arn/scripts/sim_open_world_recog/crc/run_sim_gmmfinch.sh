80612ba5cfbf7023fa0f392ad22c67b4d7561f4a
Jin Huang <kiyoshi@Jins-MacBook-Pro-2019.local>
2022-10-30 16:41:24 -0400

    debug apptainer config

--- arn/scripts/sim_open_world_recog/crc/run_sim_gmmfinch.sh
+++ arn/scripts/sim_open_world_recog/crc/run_sim_gmmfinch.sh
@@ -16,19 +16,20 @@ source activate base
 CODE_PATH="/afs/crc.nd.edu/user/j/jhuang24/Public/darpa_sail_on"
 DATA_PATH="/afs/crc.nd.edu/group/cvrl/scratch_21/dprijate/har"
 
-apptainer exec \
+apptainer exec -B /afs --no-home \
     --nv \
     --env CUDA_VISIBLE_DEVICES=$SGE_HGR_gpu_card \
-    --bind $CODE_PATH:/tmp/har/code \
-    --bind $DATA_PATH:/tmp/har/data \
-    --pwd /tmp/har/code \
+    --bind $CODE_PATH:/afs/crc.nd.edu/user/j/jhuang24/har/code \
+    --bind $DATA_PATH:/afs/crc.nd.edu/user/j/jhuang24/har/data \
+    --bind /afs/crc.nd.edu/group/cvrl/scratch_31:/afs/crc.nd.edu/user/j/jhuang24/scratch_31 \
+    --pwd /afs/crc.nd.edu/user/j/jhuang24/har/code \
     $CODE_PATH/arn/arn/data/arn_latest.sif \
     bash -c "ls -lh ./ &&
         /usr/bin/which python &&
         ls /arn
-        pip install --no-deps -e ./arn/ &&
-        pip install --no-deps -e ./exputils/ &&
-        docstr arn/arn/scripts/exp2/configs/tsf_gmmfinch_blur.yaml \
+        pip install -e ./arn/ &&
+        pip install exputils==0.1.7 &&
+        docstr arn/arn/scripts/exp2/configs/visual_transforms/x3d_gmmfinch_blur.yaml \
             --feedback_amount 1.0 \
             --predictor.min_error_tol 0.05
     "
